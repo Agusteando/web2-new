56960a0b8f63b30c57e2d853976c0b12fda8d80c
Agusteando <desarrollo.tecnologico@casitaiedis.edu.mx>
2026-03-31 20:10:26 -0600

    style(header): apply precise gradient background and pure white icon/text styling to top contact bar matching brand design request

--- components/SiteHeader.vue
+++ components/SiteHeader.vue
@@ -12,24 +12,24 @@
             <div class="iecs-topbar-inner">
               <div class="iecs-topbar-socials">
                 <ul>
-                  <li><a href="https://www.facebook.com/profile.php?id=61574164795795" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-facebook"></i></a></li>
-                  <li><a href="https://www.instagram.com/iecsi_edis/" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-instagram"></i></a></li>
-                  <li><a href="https://www.tiktok.com/@iecs.iedis" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-tiktok"></i></a></li>
-                  <li><a href="https://mx.linkedin.com/company/iecs-iedis" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-linkedin"></i></a></li>
+                  <li><a href="https://www.facebook.com/profile.php?id=61574164795795" target="_blank" rel="noopener noreferrer" aria-label="Facebook"><i class="fa-brands fa-facebook"></i></a></li>
+                  <li><a href="https://www.instagram.com/iecsi_edis/" target="_blank" rel="noopener noreferrer" aria-label="Instagram"><i class="fa-brands fa-instagram"></i></a></li>
+                  <li><a href="https://www.tiktok.com/@iecs.iedis" target="_blank" rel="noopener noreferrer" aria-label="TikTok"><i class="fa-brands fa-tiktok"></i></a></li>
+                  <li><a href="https://mx.linkedin.com/company/iecs-iedis" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><i class="fa-brands fa-linkedin"></i></a></li>
                 </ul>
               </div>
               <div class="iecs-topbar-contacts">
                 <ul>
                   <li>
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3 14C11.9111 14 10.5389 13.6972 9.18333 13.0917C7.82778 12.4861 6.59444 11.6278 5.48333 10.5167C4.37222 9.40556 3.51389 8.17222 2.90833 6.81667C2.30278 5.46111 2 4.08889 2 2.7C2 2.5 2.06667 2.33333 2.2 2.2C2.33333 2.06667 2.5 2 2.7 2H5.4C5.55556 2 5.69444 2.05278 5.81667 2.15833C5.93889 2.26389 6.01111 2.38889 6.03333 2.53333L6.46667 4.86667C6.48889 5.04444 6.48333 5.19444 6.45 5.31667C6.41667 5.43889 6.35556 5.54444 6.26667 5.63333L4.65 7.26667C4.87222 7.67778 5.13611 8.075 5.44167 8.45833C5.74722 8.84167 6.08333 9.21111 6.45 9.56667C6.79444 9.91111 7.15556 10.2306 7.53333 10.525C7.91111 10.8194 8.31111 11.0889 8.73333 11.3333L10.3 9.76667C10.4 9.66667 10.5306 9.59167 10.6917 9.54167C10.8528 9.49167 11.0111 9.47778 11.1667 9.5L13.4667 9.96667C13.6222 10.0111 13.75 10.0917 13.85 10.2083C13.95 10.325 14 10.4556 14 10.6V13.3C14 13.5 13.9333 13.6667 13.8 13.8C13.6667 13.9333 13.5 14 13.3 14ZM4.01667 6L5.11667 4.9L4.83333 3.33333H3.35C3.40556 3.78889 3.48333 4.23889 3.58333 4.68333C3.68333 5.12778 3.82778 5.56667 4.01667 6ZM9.98333 11.9667C10.4167 12.1556 10.8583 12.3056 11.3083 12.4167C11.7583 12.5278 12.2111 12.6 12.6667 12.6333V11.1667L11.1 10.85L9.98333 11.9667Z" fill="currentColor"/></svg>
-                    Metepec: <a href="tel:7221095789">722 109 5789</a>
+                    <i class="fa-light fa-phone"></i>
+                    <span>Metepec: <a href="tel:7221095789">722 109 5789</a></span>
                   </li>
                   <li>
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.3 14C11.9111 14 10.5389 13.6972 9.18333 13.0917C7.82778 12.4861 6.59444 11.6278 5.48333 10.5167C4.37222 9.40556 3.51389 8.17222 2.90833 6.81667C2.30278 5.46111 2 4.08889 2 2.7C2 2.5 2.06667 2.33333 2.2 2.2C2.33333 2.06667 2.5 2 2.7 2H5.4C5.55556 2 5.69444 2.05278 5.81667 2.15833C5.93889 2.26389 6.01111 2.38889 6.03333 2.53333L6.46667 4.86667C6.48889 5.04444 6.48333 5.19444 6.45 5.31667C6.41667 5.43889 6.35556 5.54444 6.26667 5.63333L4.65 7.26667C4.87222 7.67778 5.13611 8.075 5.44167 8.45833C5.74722 8.84167 6.08333 9.21111 6.45 9.56667C6.79444 9.91111 7.15556 10.2306 7.53333 10.525C7.91111 10.8194 8.31111 11.0889 8.73333 11.3333L10.3 9.76667C10.4 9.66667 10.5306 9.59167 10.6917 9.54167C10.8528 9.49167 11.0111 9.47778 11.1667 9.5L13.4667 9.96667C13.6222 10.0111 13.75 10.0917 13.85 10.2083C13.95 10.325 14 10.4556 14 10.6V13.3C14 13.5 13.9333 13.6667 13.8 13.8C13.6667 13.9333 13.5 14 13.3 14ZM4.01667 6L5.11667 4.9L4.83333 3.33333H3.35C3.40556 3.78889 3.48333 4.23889 3.58333 4.68333C3.68333 5.12778 3.82778 5.56667 4.01667 6ZM9.98333 11.9667C10.4167 12.1556 10.8583 12.3056 11.3083 12.4167C11.7583 12.5278 12.2111 12.6 12.6667 12.6333V11.1667L11.1 10.85L9.98333 11.9667Z" fill="currentColor"/></svg>
-                    Toluca / Calimaya / Ocoyoacac: <a href="tel:7225725729">722 572 5729</a>
+                    <i class="fa-light fa-phone"></i>
+                    <span>Toluca / Calimaya / Ocoyoacac: <a href="tel:7225725729">722 572 5729</a></span>
                   </li>
                   <li>
-                    <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M5.71249 6.71613L1 4.44814C4.42407 2.73796 8.14419 1.57183 12 1C11.3711 4.50526 10.0885 7.88719 8.2076 11L5.71249 6.71613ZM5.71249 6.71613L8.75778 3.94758" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"/></svg>
+                    <i class="fa-light fa-paper-plane"></i>
                     <a href="mailto:mercadotecnia@casitaiedis.edu.mx">mercadotecnia@casitaiedis.edu.mx</a>
                   </li>
                 </ul>
@@ -146,9 +146,9 @@ const isSearchOpen = useState('isSearchOpen', () => false)
 
 /* TOP BAR (Desktop Only) */
 .iecs-topbar {
-  border-bottom: 1px solid rgba(0, 0, 0, 0.08);
+  background: linear-gradient(90deg, #618B2F 0%, #007F92 100%);
   padding: 10px 0;
-  color: var(--tp-common-black-6, #444);
+  color: #ffffff;
 }
 
 .iecs-topbar-inner {
@@ -160,37 +160,56 @@ const isSearchOpen = useState('isSearchOpen', () => false)
 .iecs-topbar-socials ul,
 .iecs-topbar-contacts ul {
   display: flex;
-  gap: 24px;
   list-style: none;
   margin: 0;
   padding: 0;
   align-items: center;
 }
 
+.iecs-topbar-socials ul {
+  gap: 20px;
+}
+
 .iecs-topbar-socials a {
-  color: inherit;
-  transition: opacity 0.2s;
+  color: #ffffff;
+  font-size: 17px;
+  display: flex;
+  align-items: center;
+  transition: opacity 0.2s ease;
 }
 
 .iecs-topbar-socials a:hover {
-  opacity: 0.7;
+  opacity: 0.8;
+}
+
+.iecs-topbar-contacts ul {
+  gap: 32px;
 }
 
 .iecs-topbar-contacts li {
   display: flex;
   align-items: center;
-  gap: 8px;
+  gap: 10px;
   font-family: 'Montserrat', sans-serif;
   font-size: 14px;
   font-weight: 500;
+  letter-spacing: 0.3px;
+}
+
+.iecs-topbar-contacts i {
+  font-size: 15px;
+  display: flex;
+  align-items: center;
 }
 
 .iecs-topbar-contacts a {
-  color: inherit;
+  color: #ffffff;
+  text-decoration: none;
+  transition: opacity 0.2s ease;
 }
 
-.iecs-topbar-contacts svg {
-  color: var(--tp-theme-1, #618B2F);
+.iecs-topbar-contacts a:hover {
+  opacity: 0.8;
 }
 
 /* MAIN BAR */
